571223b1d9223aed3ea49113adacbf829a95f7dc
Dawid <ybt@ybteam.pl>
2017-06-13 20:25:56 +0200

    Operacje arytmetyczne

---
+++ ZmienneOperacjeArytmetyczne/ZmienneOperacjeArytmetyczne/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ZmienneOperacjeArytmetyczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Operacje arytmetyczne
+             * + dodawnie
+             * - odejmowanie
+             * * mnożenie
+             * / dzielenie
+             * % modulo
+             * = przypisanie
+             * ++ inkrementacja
+             * -- dekrementacja
+             */
+
+            int zmienna1 = 20, zmienna2 = 3;
+
+            Console.WriteLine(zmienna1 + " + " + zmienna2 + " = " + (zmienna1 + zmienna2));
+            Console.WriteLine(zmienna1 + " - " + zmienna2 + " = " + (zmienna1 - zmienna2));
+            Console.WriteLine(zmienna1 + " / " + zmienna2 + " = " + (zmienna1 / zmienna2));
+            Console.WriteLine(zmienna1 + " * " + zmienna2 + " = " + (zmienna1 * zmienna2));
+            Console.WriteLine(zmienna1 + " % " + zmienna2 + " = " + (zmienna1 % zmienna2));
+
+            // Post inkrementacja i dekrementacja
+            Console.WriteLine(zmienna1++ + " Zmienna1 inkrementacja");
+            Console.WriteLine(zmienna2-- + " Zmienna2 dekrementacja");
+
+            // Restowanie do wartosci poczatkowych
+            zmienna1 = 20;
+            zmienna2 = 3;
+
+            // Pre inkrementacja i dekrementacja
+            Console.WriteLine(++zmienna1 + " Zmienna1 inkrementacja");
+            Console.WriteLine(--zmienna2 + " Zmienna2 dekrementacja");
+
+            // Operatory przpisania
+            zmienna1 = 20;
+            zmienna2 = 3;
+            zmienna1 += zmienna2;
+            Console.WriteLine(" += " + zmienna1);
+
+            zmienna1 = 20;
+            zmienna2 = 3;
+            zmienna1 -= zmienna2;
+            Console.WriteLine(" -= " + zmienna1);
+
+            zmienna1 = 20;
+            zmienna2 = 3;
+            zmienna1 /= zmienna2;
+            Console.WriteLine(" /= " + zmienna1);
+
+            zmienna1 = 20;
+            zmienna2 = 3;
+            zmienna1 *= zmienna2;
+            Console.WriteLine(" *= " + zmienna1);
+
+            zmienna1 = 20;
+            zmienna2 = 3;
+            zmienna1 %= zmienna2;
+            Console.WriteLine(" %= " + zmienna1);
+            Console.ReadKey();
+        }
+    }
+}

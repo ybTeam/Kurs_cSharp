8802613701f46276d8a5b4e98477a573af82239f
Dawid <ybt@ybteam.pl>
2017-06-13 20:31:46 +0200

    Operatory relacji

---
+++ OperatoryRelacyjne/OperatoryRelacyjne/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatoryRelacyjne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba1 = 15, liczba2 = 30;
+            
+            if(liczba1 == liczba2)
+            {
+                Console.WriteLine("Liczba1 jest rowna Liczba2");
+            }
+
+            if(liczba1 != liczba2)
+            {
+                Console.WriteLine("Liczba1 jest rozna od Liczba2");
+            }
+
+            if(liczba1 > liczba2)
+            {
+                Console.WriteLine("Liczba1 jest wieksze od Liczba2");
+            }
+
+            if (liczba1 < liczba2)
+            {
+                Console.WriteLine("Liczba1 jest mniejsza od Liczba2");
+            }
+
+            if (liczba1 >= liczba2)
+            {
+                Console.WriteLine("Liczba1 jest wieksze badz rowne Liczba2");
+            }
+
+            if (liczba1 <= liczba2)
+            {
+                Console.WriteLine("Liczba1 jest mniejsze badz rowne Liczba2");
+            }
+        }
+    }
+}
